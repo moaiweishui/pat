060c89af2c856e90ae201960038bec4826bb8825
LiuSS <liuyuanxiu1218@gmail.com>
2017-12-02 22:17:47 +0800

    Update src.cpp

--- 1121. Damn Single/src.cpp
+++ 1121. Damn Single/src.cpp
@@ -1,93 +1,65 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
+#include <functional>
+#include <math.h>
+#include <limits.h>
+#include <float.h>
 #include <sstream>
+#include <string>
+#include <string.h>
+
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
-bool cmp(string a, string b)
-{
-    int x, y;
-    stringstream s1, s2;
-    s1 << a;
-    s2 << b;
-    s1 >> x;
-    s2 >> y;
-    return x < y;
-}
-
-int string2int(string s)
-{
-    int res;
-    stringstream ss;
-    ss << s;
-    ss >> res;
-
-    return res;
-}
-
-int main()
-{
-    int n;
-    cin >> n;
-
-    vector<int> couple(100000, -1);
-    vector<bool> guests(100000, false);
-
-    int buf1, buf2;
-
-    unordered_map<int, string> id;
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> buf1;
-        cin >> buf2;
-        couple[buf1] = buf2;
-        couple[buf2] = buf1;
+int main() {
+   
+    vector<int> couples(100000, -1);
+    int N, M;
+    cin >> N;
+    int id1, id2;
+    for (int i = 0; i < N; i++){
+        scanf("%d %d", &id1, &id2);
+        couples[id1] = id2;
+        couples[id2] = id1;
     }
 
-    int m;
-    cin >> m;
-    vector<int> people;
-
-    string buf;
-    int x;
-
-    for (int i = 0; i < m; i++)
-    {
-        cin >> buf;
-        x = string2int(buf);
-        id[x] = buf;
-        people.push_back(x);
-        guests[x] = true;
-    }
-
-    vector<int> res;
-
-    for (int i = 0; i < m; i++)
-    {
-        if (couple[people[i]] == -1)
-            res.push_back(people[i]);
-        else if (!guests[couple[people[i]]])
-        {
-            res.push_back(people[i]);
+    cin >> M;
+    int id;
+    vector<bool> party(100000, false);
+    set<int> single_set;
+    int single = 0;
+    for (int i = 0; i < M; i++){
+        scanf("%d", &id);
+        party[id] = true;
+        if (couples[id] == -1){      
+            single_set.insert(id);
+        }
+        else{
+            if (party[couples[id]] == true){
+                single_set.erase(couples[id]);
+            }
+            else{
+                single_set.insert(id);
+            }
         }
     }
 
-    sort(res.begin(), res.end());
-
-    cout << res.size() << endl;
-
-    for (int i = 0; i < res.size(); i++)
-    {
-        if (i != res.size() - 1)
-            cout << id[res[i]] << ' ';
+    cout << single_set.size() << endl;
+    for (auto it = single_set.begin(); it != single_set.end(); it++){
+        if (it == single_set.begin())
+            printf("%05d", *it);
         else
-            cout << id[res[i]] << endl;
+            printf(" %05d", *it);
     }
-
+    cout << endl;
     return 0;
 }
